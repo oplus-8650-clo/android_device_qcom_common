0cfee9dfbc33f17836eace3d6c2724617cff57ce
anky894 <anky894@proton.me>
2026-07-22 21:30:28 +0530

    vendor: adreno: Enable Vulkan for pineapple
    Change-Id: I3fc384bf6b07e19bbf70720d4974228c69a33a8c

--- vendor/adreno/u/qti-adreno.mk
+++ vendor/adreno/u/qti-adreno.mk
@@ -8,3 +8,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_3.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+
+# Vulkan
+TARGET_USES_VULKAN := true
